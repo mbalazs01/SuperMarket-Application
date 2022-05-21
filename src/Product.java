82f66aa74cc3dc5eed61dfd3267bf430a45a4376
mbalazs01 <makaibalazs01@gmail.com>
2022-05-21 04:24:16 +0200

    Products class fully implemented, now reads from Database

--- src/Product.java
+++ src/Product.java
@@ -4,10 +4,10 @@ public class Product extends ProductBase {
     public Product(String name, int price, int productID, String categories, String ids) {
         super(name, price, productID);
         this.categories = categories;
-        this.categoriesList = uggghhhh(categories, ids);
+        this.categoriesList = convertToList(categories, ids);
     }
 
-    private List<Category> uggghhhh(String categories, String categoryIDs) {
+    private List<Category> convertToList(String categories, String categoryIDs) {
         List<String> categoryNames = Arrays.asList(categories.split("#"));
         List<String> ids = Arrays.asList(categoryIDs.split("#"));
         List<Category> resultingList = new ArrayList<>();
